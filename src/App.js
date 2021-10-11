9e3b301355b3f7866524a0654a1bb7797b3d8664
Iliya Lunev <lunevig90@gmail.com>
2021-10-11 16:19:09 +0300

    add localStorage

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import s from './componets/FormPhoneBook/formPhone.module.css';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -46,6 +41,21 @@ export default class App extends Component {
   handleContactsFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
+  // Добовляем в LocalStorage
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  // Просматриваем на странице
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   render() {
     const visibleContacts = this.getVisibleContacts();
